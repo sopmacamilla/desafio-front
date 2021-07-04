67af2685bedc6cb131311ea3503c26bc6879bc3f
sopmacamilla <camillaoui@gmail.com>
2021-07-04 15:01:43 -0300

    Inserido html dos componentes e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroPedidoComponent } from './cadastro-pedido/cadastro-pedido.component';
+import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'pagina-inicial', pathMatch: 'full'},
+  {path: 'pagina-inicial', component: PaginaInicialComponent},
+  {path: 'cadastrar-servico', component: CadastroServicoComponent},
+  {path: 'cadastrar-pedido', component: CadastroPedidoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
+import { CadastroPedidoComponent } from './cadastro-pedido/cadastro-pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
+    CadastroServicoComponent,
+    CadastroPedidoComponent
   ],
   imports: [
     BrowserModule,
